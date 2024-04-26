d524f6c64963c34a6f8507f844c2c68a30433871
jkstarling <beets09@yahoo.com>
2024-04-25 22:00:33 -0400

    25APR24 10PM

--- RP_SL_app/pages/2_💰_Profit and Loss.py
+++ RP_SL_app/pages/2_💰_Profit and Loss.py
@@ -13,7 +13,7 @@ df = st.session_state["dataframe"]
 # df['date2'] = pd.to_datetime(df['date'], format='%b %Y').dt.to_period('M')
 
 df_pl = df[df['report'].str.contains("PL")]
-# df_cf_netincome = df_cf[df_cf.rowname=='Net Income']
+df_pl_membrev = df_pl[df_pl.rowname=='Membership Revenue']
 
 
 pivot_table = pd.pivot_table(df_pl, 
@@ -30,15 +30,31 @@ st.write("""
 # st.dataframe(pivot_table)
 st.dataframe(pivot_table.drop('ind_row', axis=1))
 
+fig = go.Figure()
 
-# Create Plotly figure
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(x=df_cf_netincome.date, y=df_cf_netincome.cost, mode='lines+markers', name='Net Income'))
-# fig.update_layout(title='Net Income Over Time',
-#                   xaxis_title='Date',
-#                   yaxis_title='Net Income',
-#                   xaxis_tickangle=-45,
-#                   showlegend=True)
+# Assuming df_pl_membrev is your DataFrame and 'date' is in the "MMM-YY" format
+df_pl_membrev['date'] = pd.to_datetime(df_pl_membrev['date'], format='%b-%y')
 
-# # Display the Plotly figure in Streamlit
-# st.plotly_chart(fig)
+# Add a trace for each unique value in 'report'
+for report_value in df_pl_membrev['report'].unique():
+    df_report = df_pl_membrev[df_pl_membrev['report'] == report_value]
+    fig.add_trace(go.Scatter(
+        x=df_report['date'],
+        y=df_report['cost'],
+        mode='lines+markers',
+        name=report_value,
+        line=dict(width=2),
+        marker=dict(size=8),
+    ))
+
+fig.update_layout(
+    title='Membership Revenue by Location',
+    xaxis_title='Date',
+    yaxis_title='Revenue',
+    xaxis_tickangle=-45,
+    showlegend=True,
+)
+
+# Display the Plotly figure in Streamlit
+st.plotly_chart(fig)
+# fig.show()
