5211beb634660ee75ff3628496c3077b923b2fd6
jkstarling <beets09@yahoo.com>
2024-05-04 18:28:26 -0400

    4MAY2024 Update

--- RP_SL_app/Remedy Place SL App.py
+++ RP_SL_app/Remedy Place SL App.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.set_page_config(page_title = "Remedy Place - Streamlit Example Webpages", page_icon = "♾️" )
 
 st.write("""
         # Remedy Place - Streamlit Example Webpages
 
-        This website will highlight a handful of Streamlit possibilities that will help Remedy Place be financially sound.
+        This website will highlight a handful of Streamlit possibilities for Remedy Place and others.
         """)
-
+ 
 
 # myfile = st.sidebar.file_uploader("RP_example_data_1-23_3_24.xlsx", type=["xlsx"])
 
+# os.chdir(r"G:/My Drive/kordis/SL_example/PythonStreamlit")
 df = pd.read_csv('G:\My Drive\kordis\Streamlit_Example_RP\RP_SL_app\RP_example_data_1-23_3_24.csv')
 
 # Convert 'date' column to datetime format
---
+++ RP_SL_app/run_conda.sh
@@ -0,0 +1,7 @@
+#!/bin/bash    ##### https://saturncloud.io/blog/calling-conda-source-activate-from-bash-script-a-guide/
+
+source activate kordis_py
+
+# Your commands here
+
+conda deactivate
